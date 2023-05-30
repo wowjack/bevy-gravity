5aeb1c0adc065d8dc704ee763df43c566cfa97c5
jack kingham <wowknead@gmail.com>
2023-05-30 13:35:02 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use bevy::{prelude::*, core_pipeline::clear_color::ClearColorConfig};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(init)
+        .run()
+}
+
+fn init(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        camera_2d: Camera2d {clear_color: ClearColorConfig::Custom(Color::BEIGE)},
+        ..Camera2dBundle::default()
+    });
+}
